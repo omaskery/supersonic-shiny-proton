e3ab53689d6138df460da097b3d911cb64fa0dbc
Oliver Maskery <omaskery@googlemail.com>
2016-04-05 20:16:50 +0100

    refactored the opcode out of the assembler and into the top level scripting module so it can be reused in the virtual machine

---
+++ ssp/scripting/opcode.py
@@ -0,0 +1,65 @@
+
+
+class Opcode:
+	NOP = 0
+	PUSH = 1
+	SEND = 2
+	SWAP = 3
+	DUP = 4
+	APPEND = 5
+	ADD = 6
+	SUB = 7
+	MUL = 8
+	DIV = 9
+	RECV = 10
+	LISTEN = 11
+	DICT = 12
+	LIST = 13
+	PUT = 14
+	LOOKUP = 15
+	LEN = 16
+
+	@classmethod
+	def from_string(cls, string):
+		return {
+			'NOP': cls.NOP,
+			'PUSH': cls.PUSH,
+			'SEND': cls.SEND,
+			'SWAP': cls.SWAP,
+			'DUP': cls.DUP,
+			'APPEND': cls.APPEND,
+			'ADD': cls.ADD,
+			'SUB': cls.SUB,
+			'MUL': cls.MUL,
+			'DIV': cls.DIV,
+			'RECV': cls.RECV,
+			'LISTEN': cls.LISTEN,
+			'DICT': cls.DICT,
+			'LIST': cls.LIST,
+			'PUT': cls.PUT,
+			'LOOKUP': cls.LOOKUP,
+			'LEN': cls.LEN,
+		}[string.upper()]
+
+	@classmethod
+	def to_string(cls, integer):
+		return {
+			cls.NOP: 'NOP',
+			cls.PUSH: 'PUSH',
+			cls.SEND: 'SEND',
+			cls.SWAP: 'SWAP',
+			cls.DUP: 'DUP',
+			cls.APPEND: 'APPEND',
+			cls.ADD: 'ADD',
+			cls.SUB: 'SUB',
+			cls.MUL: 'MUL',
+			cls.DIV: 'DIV',
+			cls.RECV: 'RECV',
+			cls.LISTEN: 'LISTEN',
+			cls.DICT: 'DICT',
+			cls.LIST: 'LIST',
+			cls.PUT: 'PUT',
+			cls.LOOKUP: 'LOOKUP',
+			cls.LEN: 'LEN',
+		}[integer]
+
